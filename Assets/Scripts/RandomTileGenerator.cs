8ad42246ecb0faa8cbdcb342b54e5381dd8f60a1
Emre ŞİŞMAN <emresisman53@gmail.com>
2022-04-18 15:48:47 +0300

    random tile generator finished

--- Assets/Scripts/RandomTileGenerator.cs
+++ Assets/Scripts/RandomTileGenerator.cs
@@ -7,23 +7,62 @@ namespace emresisman
 {
     public class RandomTileGenerator : MonoBehaviour
     {
-        Dictionary<int, Tile> tiles = new Dictionary<int, Tile>();
+        public const int SCREEN_SIZE_TILE_COUNT = 30;
+        private int _createdTileCount;
+
+        private float _playerSpeed;
+        private Vector3Int _currentHorizontalPosition;
         
         public Tilemap _mainTileMap;
         public Tile _tile;
-        int _index = 0;
-        
 
-        private void Update()
+
+        private void Start()
         {
-            SetTile(_tile);
-            
+            _currentHorizontalPosition = new Vector3Int(0, 0, 0);
         }
 
         void SetTile(Tile _tile)
         {
-            _mainTileMap.SetTile(new Vector3Int(_index++, 0, 0), null);
+            _mainTileMap.SetTile(_currentHorizontalPosition, _tile);
+            GoNextPosition();
         }
+
+        private void GoNextPosition()
+        {
+            _currentHorizontalPosition =
+                new Vector3Int(_currentHorizontalPosition.x + 1, 0, 0);
+                
+        }
+
+        private void CreateNewScreenTiles()
+        {
+            _createdTileCount = 0;
+            while (_createdTileCount < SCREEN_SIZE_TILE_COUNT)
+            {
+                _createdTileCount += CreatePath() + CreateSpace();
+            }
+        }
+
+        private int CreatePath()
+        {
+            int i = Random.Range(1, ((int)_playerSpeed) * 5);
+            for (int k = 0; k < i; k++)
+            {
+                SetTile(_tile);
+            }
+            return i;
+        }
+
+        private int CreateSpace()
+        {
+            int i = Random.Range(1, ((int)_playerSpeed));
+            for (int k = 0; k < i; k++)
+            {
+                SetTile(null);
+            }
+            return i;
+        }
+
     }
 }
-
